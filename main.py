6aeb02989319bf68ab664bffb00b77afbb5aa556
RangerVinven <daniel.mcpherson@live.co.uk>
2024-12-24 15:44:16 +0100

    Added functions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import re
+import os
 import html
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
 
-def get_stitch_html_css(stitch_id):
+def get_page_html(stitch_id):
     url = "https://codestitch.app/app/dashboard/stitches/" + str(stitch_id)
     response = requests.get(url)
 
@@ -15,6 +16,10 @@ def get_stitch_html_css(stitch_id):
 
     # Gets the entire page's contents
     page_html = response.content.decode()
+    return page_html
+
+def get_stitch_html_css(stitch_id):
+    page_html = get_page_html(stitch_id)
 
     # Creates the parser and extracts the HTML
     parser = BeautifulSoup(page_html, "html.parser")
@@ -27,22 +32,9 @@ def get_stitch_html_css(stitch_id):
     # Decodes the HTML (it's recieved URL encoded)
     stitch_html = html.unescape(stitch_html_encoded[0].text)
 
-    # Gets the CSS navbar item
-    # stitch_navbar_items = parser.select("code_list__item")
-    stitch_navbar_items = parser.find_all("li", class_="code_list__item")
-
-    # If we couldn't get the css navbar item
-    if len(stitch_navbar_items) < 2:
-        raise Exception("Couldn't get the CSS navbar item")
-
-    # Gets the a tag with the data-codeid attribute
-    stitch_css_item = stitch_navbar_items[-2].find("a")
-
-    if "data-codeid" not in stitch_css_item.attrs:
-        raise Exception("Couldn't get the CSS's codeid (used to find the codeblock for the CSS)")
-
     # Gets the code_id
-    code_id = stitch_css_item["data-codeid"]
+    css_a_tag = parser.find("a", class_="code_list_link", attrs={"data-codetype": "css"})
+    code_id = css_a_tag["data-codeid"]
 
     # Gets the div with the textarea child with the css
     parent_div = parser.find_all("div", class_="tab", attrs={"data-codeid": code_id})
@@ -54,8 +46,29 @@ def get_stitch_html_css(stitch_id):
     stitch_css_encoded = parent_div[0].find("textarea")
     stitch_css = html.unescape(stitch_css_encoded.text)
 
-    return (stitch_html, stitch_css)
-    
+    # Gets the core styles
+    css_a_tag = parser.find("a", class_="code_list_link", attrs={"data-codetype": "core-styles"})
+
+    return [stitch_html, stitch_css]
+
+def get_core_styles(stitch_id):
+    page_html = get_page_html(stitch_id)
+
+    # Initalises the parser
+    parser = BeautifulSoup(page_html, "html.parser")
+
+    # Creates the parent_div
+    parent_div = parser.find_all("div", class_="tab", attrs={"data-codeid": "core-styles-CSS"})
+    core_styles_encoded = parent_div[0].find("textarea")
+
+    # Decodes the core_styles
+    core_styles = html.unescape(core_styles_encoded.text)
+
+    # Saves it to the root.css file
+    with open("website/src/assets/css/root.css", "a") as f:
+        f.write(core_styles)
+
+
 # Gets a list of the stitches
 def get_stitches(stitches):
     stitches_code = []
@@ -66,7 +79,7 @@ def get_stitches(stitches):
         stitches_code.append(code)
 
         print("Got {}!".format(stitch))
-        sleep(0.5)
+        # sleep(0.5)
 
     return stitches_code
 
@@ -75,14 +88,27 @@ def create_page(page_name, stitches):
     if page_name == "index":
         create_index_page(page_name, stitches)
 
+
 # Creates an index page
 def create_index_page(page_name, stitches):
     # Gets all the website's stitches
     stitches_code = get_stitches(stitches)
-    save_to_file(stitches_code)
+    save_to_file(page_name, stitches_code)
 
-def save_to_file(html_and_css):
-    with open("index.html", "a") as f:
+def save_to_file(page_name, html_and_css):
+    # Sets the path of the HTML file
+    html_file_path = ""
+    css_file_path = ""
+    if page_name == "index":
+        html_file_path += "website/src/" + page_name + ".html"
+        css_file_path += "website/src/assets/css/" + "local.css"
+
+    else:
+        html_file_path += "website/src/content/pages/" + page_name + ".html"
+        css_file_path += "website/src/assets/css/" + page_name + ".css"
+
+    # Saves the HTML to the file
+    with open(html_file_path, "a") as f:
         f.write("{% block body %}\n")
 
         # Writes the HTML to the index file
@@ -91,7 +117,25 @@ def save_to_file(html_and_css):
 
         f.write("\n{% endblock %}\n")
 
-# code = get_stitch_html_css(2051)
-# print(code)
+    # If the file is a home page, saves the hero's CSS to the critical file
+    if page_name == "index":
+        with open("website/src/assets/css/critical.css", "a") as f:
+            f.write("\n")
+            f.write(html_and_css[0][1])
+            f.write("\n")
 
-create_page("index", [1785, 1666, 1446])
+        # Removes the hero's code from the array
+        html_and_css.pop(0)
+
+    with open(css_file_path, "a") as f:
+        for html_and_css_code in html_and_css:
+            f.write(html_and_css_code[1])
+            f.write("\n")
+
+    print("Created website!")
+
+# os.system("rm -r website;cp -r backup_website website")
+stitches = [1785, 1666, 1446]
+get_core_styles(stitches[0])
+create_page("index", stitches)
+# create_page("index", [1785, 1666])
