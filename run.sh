eb08f7cbf78db43caaa4732f989543637c135f00
RangerVinven <daniel.mcpherson@live.co.uk>
2024-12-25 01:28:41 +0100

    Added git remote remove origin

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ source venv/bin/activate
 yes | rm -r website
 git clone https://github.com/RangerVinven/Intermediate-SASS-CodeStitch-Fork.git website
 cd website
+git remote remove origin
 npm install
 cd ../
 python3 main.py
